bdd3d9e745ab16fd515c9a36f7cc5be9200ffb3f
ubaldino zurita <ozzirisc@gmail.com>
2014-08-01 16:05:14 +0000

    mvc declared

--- grabador.py
+++ grabador.py
@@ -1,18 +1,39 @@
-import usb.core
-dev = usb.core.find()
-print( dev.bDescriptorType )
+import wx , os , subprocess
+
+BASE_DIR = os.path.dirname( __file__)
+
+
+
+
+
 """
- |  Additionally, the class provides methods to communicate with
- |  the hardware. Typically, an application will first call the
- |  set_configuration() method to put the device in a known configured
- |  state, optionally call the set_interface_altsetting() to select the
- |  alternate setting (if there is more than one) of the interface used,
- |  and call the write() and read() method to send and receive data.
- |  When working in a new hardware, one first try would be like this:
- """
-#dev = usb.core.find(idVendor=myVendorId, idProduct=myProductId)
-#dev.set_configuration()
-#dev.write(1, 'test')
+print( "Ejecutando bootloader" )
 
+salida = subprocess.call( [ BASE_DIR+"/hid_bootloaderp" , BASE_DIR+"/BLINK.hex" ] )
+subprocess.check_output(["echo", "Hello World!"], universal_newlines=True)
+'Hello World!\n'
+"""
 
+class Vista( wx.Frame ):
+  
+    def __init__(self, parent, title):
+        super( Vista , self ).__init__( parent , title=title , size=(300, 200) )
+        
+        #self.Background( "#f13");    
+        self.Centre()
+        self.Show()
 
+class Controlador(object):
+	def __init__(self):
+		vista = Vista( None , title='picusb' ) 
+		modelo = Modelo()
+		
+class Modelo(object):
+	def __init__(self ):
+		self.list_pics = []
+		
+
+if __name__ == '__main__':
+    app = wx.App()
+    Controlador()
+    app.MainLoop()
